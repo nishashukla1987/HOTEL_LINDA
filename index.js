f26c023230accdfee9c47e4a7e229bfc9ffb6cf8
Nisha <nishashukla1987@gmail.com>
2021-06-09 20:47:09 +0200

    addedresponsive navbar and gallery option also

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-const navList = document.querySelector(".nav__list");
-const burgerMenu = document.querySelector(".burger");
+//FORM
+var form = document.getElementById('my-form');
 
-burgerMenu.addEventListener("click", () => {
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById('my-form-status');
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => {
+      status.innerHTML = 'Thanks for your submission!';
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = 'Oops! There was a problem submitting your form';
+    });
+}
+form.addEventListener('submit', handleSubmit);
+
+//NAVBAR
+const navList = document.querySelector('.nav__list');
+const burgerMenu = document.querySelector('.burger');
+
+burgerMenu.addEventListener('click', () => {
   // Add inactive class for smooth transition
-  if (navList.classList.contains("active")) {
-    navList.classList.add("inactive");
+  if (navList.classList.contains('active')) {
+    navList.classList.add('inactive');
     setTimeout(() => {
-      navList.classList.remove("inactive");
-      navList.classList.toggle("active");
+      navList.classList.remove('inactive');
+      navList.classList.toggle('active');
     }, 100);
-  } else navList.classList.toggle("active");
+  } else navList.classList.toggle('active');
 
-  window.addEventListener("resize", removeClasses);
-  burgerMenu.classList.toggle("active");
+  window.addEventListener('resize', removeClasses);
+  burgerMenu.classList.toggle('active');
 });
 
 // Remove active sidebar on resize beyond set width
 const removeClasses = () => {
   if (window.innerWidth > 672) {
-    if (navList.classList.contains("active")) navList.classList.add("inactive");
+    if (navList.classList.contains('active')) navList.classList.add('inactive');
     setTimeout(() => {
-      navList.classList.remove("inactive");
-      navList.classList.remove("active");
-      burgerMenu.classList.remove("active");
+      navList.classList.remove('inactive');
+      navList.classList.remove('active');
+      burgerMenu.classList.remove('active');
     }, 100);
-    window.removeEventListener("resize", removeClasses);
+    window.removeEventListener('resize', removeClasses);
   }
 };
+
+//-------------gallery
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('img');
+images.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(img);
+  });
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove('active');
+});
--- navbar.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector('.menu');
-const ul = document.querySelector('ul');
-menu.onclick = function () {
-  ul.classList.toggle('active');
-};
