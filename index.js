f15e424163b7d12fdb3aa8d5b397416c5cd282e5
Nisha <nishashukla1987@gmail.com>
2021-06-19 15:48:11 +0200

    adjusted uberuns height in responsive mode

--- index.js
+++ index.js
@@ -24,23 +24,6 @@ async function handleSubmit(event) {
 form.addEventListener('submit', handleSubmit);
 
 //NAVBAR
-// const navList = document.querySelector('.nav__list');
-// const burgerMenu = document.querySelector('.burger');
-
-// burgerMenu.addEventListener('click', () => {
-//   // Add inactive class for smooth transition
-//   if (navList.classList.contains('active')) {
-//     navList.classList.add('inactive');
-//     setTimeout(() => {
-//       navList.classList.remove('inactive');
-//       navList.classList.toggle('active');
-//     }, 100);
-//   } else navList.classList.toggle('active');
-
-//   window.addEventListener('resize', removeClasses);
-//   burgerMenu.classList.toggle('active');
-// });
-//----------------new navbar
 
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
